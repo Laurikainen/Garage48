f88026876ac4651e8fe7b53c1cb85dd681cdccee
Karl Kruus <karlkruus0@gmail.com>
2019-10-12 20:02:25 +0300

    Add pie charts

--- Lumino Admin Bootstrap Template/lumino/js/easypiechart-data.js
+++ Lumino Admin Bootstrap Template/lumino/js/easypiechart-data.js
@@ -25,3 +25,46 @@ $(function() {
        barColor: '#30a5ff'
    });
 });
+
+$(function() {
+    $('#easypiechart-teal2').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-orange2').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
+
+$(function() {
+    $('#easypiechart-red2').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+   $('#easypiechart-blue2').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+
+
+$(function() {
+    $('#easypiechart-blue3').easyPieChart({
+        scaleColor: false,
+        barColor: '#30a5ff'
+    });
+ });
+
+$(function() {
+    $('#easypiechart-orange3').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
