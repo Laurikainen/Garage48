fb4dcca36bbfc4ed22d931d3b6809ddc8bbdf52e
raulkolk <raulklk@gmail.com>
2019-10-12 18:50:51 +0300

    Add files via upload

---
+++ Lumino Admin Bootstrap Template/lumino/js/script.js
@@ -0,0 +1,40 @@
+
+fetch('https://growby.app/slide',{
+    headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    method: "POST",
+    body: JSON.stringify({a: 1, b: 2})})
+.then(r => r.json())
+.then(data => {
+
+//data on jada 
+
+document.getElementById('reports').innerHTML=
+
+`	<div class="col-md-4">
+<div class="panel panel-info">
+    <div class="panel-heading">Info Panel</div>
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
+    </div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-warning">
+    <div class="panel-heading">Warning Panel</div>
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
+    </div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-danger">
+    <div class="panel-heading">Danger Panel</div>
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
+    </div>
+</div>
+</div>`
+
+
+  
+});
