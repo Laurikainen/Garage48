d6a0bafec52d0334e3282e35a1a25231dc982da9
raulkolk <raulklk@gmail.com>
2019-10-12 20:34:18 +0300

    Update script.js

--- Lumino Admin Bootstrap Template/lumino/js/script.js
+++ Lumino Admin Bootstrap Template/lumino/js/script.js
@@ -5,7 +5,7 @@ fetch('https://growby.app/slide',{
 .then(r => r.json())
 .then(data => {
 
-    console.log(document.getElementsByClassName('easypiechart')[0]).setAttribute('data-percent','999');
+ document.getElementsByClassName('easypiechart')[0]).setAttribute('data-percent','999');
 
 data.forEach(el => {
 
