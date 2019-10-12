ce8d8e173a0e6605c486b72b597dd2a81ce915a8
raulkolk <raulklk@gmail.com>
2019-10-12 20:18:32 +0300

    Update script.js

--- Lumino Admin Bootstrap Template/lumino/js/script.js
+++ Lumino Admin Bootstrap Template/lumino/js/script.js
@@ -9,7 +9,7 @@ data.forEach(el => {
 
     
 
-if(el.why)document.getElementById('reports').innerHTML=
+if(el.why)document.getElementById('reports').innerHTML+=
 `	<div class="col-md-4">
 <div class="panel panel-info">
     <div class="panel-heading">Info Panel</div>
@@ -18,22 +18,7 @@ if(el.why)document.getElementById('reports').innerHTML=
     </div>
 </div>
 </div>
-<div class="col-md-4">
-<div class="panel panel-warning">
-    <div class="panel-heading">Warning Panel</div>
-    <div class="panel-body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
-    </div>
-</div>
-</div>
-<div class="col-md-4">
-<div class="panel panel-danger">
-    <div class="panel-heading">Danger Panel</div>
-    <div class="panel-body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
-    </div>
-</div>
-</div>`
+`
     
 });
 /*
