af2462756dd36ed2e8ae356663db5ad33bcb1a5f
raulkolk <raulklk@gmail.com>
2019-10-12 18:59:01 +0300

    Update script.js

--- Lumino Admin Bootstrap Template/lumino/js/script.js
+++ Lumino Admin Bootstrap Template/lumino/js/script.js
@@ -8,6 +8,39 @@ fetch('https://growby.app/slide',{
 
 //data on jada 
 
+data.forEach(el => {
+
+    
+
+if(el.why)document.getElementById('reports').innerHTML=
+`	<div class="col-md-4">
+<div class="panel panel-info">
+    <div class="panel-heading">Info Panel</div>
+    <div class="panel-body">
+        <p>`+el.why+`</p>
+    </div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-warning">
+    <div class="panel-heading">Warning Panel</div>
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
+    </div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-danger">
+    <div class="panel-heading">Danger Panel</div>
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
+    </div>
+</div>
+</div>`
+    
+});
+
+
 document.getElementById('reports').innerHTML=
 JSON.stringify(data)+
 `	<div class="col-md-4">
